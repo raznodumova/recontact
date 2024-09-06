a9ecce0074d8dbb515dbf777ba09f04b8000a06b
Полина Тригуб PD-87 <doc21@inbox.ru>
2024-09-06 20:04:31 +0300

    исправлено

--- main.py
+++ main.py
@@ -5,12 +5,11 @@ import re
 with open("phonebook_raw.csv", encoding="utf-8") as f:
     rows = csv.reader(f, delimiter=",")
     contacts_list = list(rows)
-# pprint(contacts_list)
 
 con_dict = []
 
 
-def change_names(contacts_list):
+def change_names():
     keys = contacts_list[0]
     values = contacts_list[1:]
     for k, v in enumerate(values):
@@ -19,7 +18,8 @@ def change_names(contacts_list):
             con_dict[k].update({key: val})
 
     for names in con_dict:
-        name = names['lastname'].split()
+        fullname = names['firstname'] + ' ' + names['lastname'] + ' ' + names['surname']
+        name = fullname.split()
         if len(name) == 3:
             names['lastname'] = name[0]
             names['firstname'] = name[1]
@@ -35,7 +35,7 @@ def change_names(contacts_list):
     return con_dict
 
 
-def change_phone(con_dict):
+def change_phone():
     phone_pattern = re.compile(r'(\+7|8)?\s*\(?(\d{3})\)?\s*\D?(\d{3})[-\s+]?(\d{2})-?(\d{2})((\s)?\(?(доб.)?\s?(\d+)\)?)?')
     phone_substitution = r'+7(\2)\3-\4-\5\7\8\9'
 
@@ -46,30 +46,37 @@ def change_phone(con_dict):
     return con_dict
 
 
-def merge_duplicates(con_dict):
-    merged_dict = {}
-    for con in con_dict:
-        key = (con['lastname'], con['firstname'], con['surname'])
-        if key not in merged_dict:
-            merged_dict[key] = con
-        else:
-            existing_contact = merged_dict[key]
-            for k in con.keys():
-                if con[k] and con[k] != existing_contact[k]:
-                    existing_contact[k] += f'; {con[k]}'
+def merge_duplicates(contacts):
+    merged_contacts = {}
 
-    return list(merged_dict.values())
+    for contact in contacts:
+        key = frozenset({contact['firstname'], contact['lastname']})
+
+        if key not in merged_contacts:
+            merged_contacts[key] = contact.copy()
+        else:
+            existing_contact = merged_contacts[key]
+            for field in existing_contact:
+                if existing_contact[field] == '' and contact[field] != '':
+                    existing_contact[field] = contact[field]
+
+    return list(merged_contacts.values())
+
+
+def write_to_csv(contacts, filename):
+    with open(filename, mode='w', newline='', encoding='utf-8') as file:
+        fieldnames = ['lastname', 'firstname', 'surname', 'phone', 'email', 'organization', 'position']
+
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+
+        for contact in contacts:
+            writer.writerow(contact)
 
 
 if __name__ == '__main__':
-    con_dict = change_names(contacts_list)
-    con_dict = change_phone(con_dict)
-    con_dict = merge_duplicates(con_dict)
-
-    with open("phonebook.csv", "w", encoding="utf-8", newline='') as f:
-        datawriter = csv.writer(f, delimiter=',')
-        header = ["lastname", "firstname", "surname", "phone", "email", "organization", "position"]
-        datawriter.writerow(header)
-        for contact in con_dict:
-            datawriter.writerow([contact['lastname'], contact['firstname'], contact['surname'],
-                                 contact['phone'], contact['email'], contact['organization'], contact['position']])
+    change_names()
+    change_phone()
+    pprint(con_dict)
+    merged_contacts = merge_duplicates(con_dict)
+    write_to_csv(merged_contacts, 'phonebook.csv')
